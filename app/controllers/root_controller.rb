fed6f8c0b2b1f87a8258e92cf8f40b6489e13eee
mattak <mattak.me@gmail.com>
2014-05-06 00:08:57 +0900

    improve interface to inject bootstrap.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,2 +1,7 @@
 class RootController < ApplicationController
+  def index
+    @program_count   = Program.all.count
+    @character_count = Character.all.count
+    @actor_count     = Actor.all.count
+  end
 end
